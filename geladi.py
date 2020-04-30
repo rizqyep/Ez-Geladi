b7dc33e7ebf35f1a54b0ad3a807d71d76464a2b8
Rizqy EP <rizqyepr@gmail.com>
2020-04-30 22:46:59 +0700

    worked on link

--- geladi.py
+++ geladi.py
@@ -1,13 +1,21 @@
 import time
 from time import sleep
 from selenium import webdriver
-URL = "https://igracias.telkomuniversity.ac.id/index.php"
- 
+URL = "https://igracias.telkomuniversity.ac.id"
 PATH_TO_DRIVER = 'chromedriver_win32/chromedriver.exe'
 browser = webdriver.Chrome(PATH_TO_DRIVER)
-browser.set_window_position(0,0)
-browser.get(URL)
+username = input("Input username igracias : ")
+password = input("Input password igracias : ")
 
+
+browser.get(URL)
+uname_field = browser.find_element_by_id("textUsername")
+uname_field.send_keys(username)
+pass_field  = browser.find_element_by_id("textPassword")
+pass_field.send_keys(password)
+login_btn = browser.find_element_by_id("submit")
+login_btn.click()
 while True:
-    sleep(5) #atur sesuai waktu yang diperluin buat login diawal
+    browser.get("https://igracias.telkomuniversity.ac.id/geladi")
+    sleep(10) #atur sesuai waktu yang diperluin buat login diawal
     browser.refresh()
