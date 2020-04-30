b7f7a930a18e170e207adf4b9656778f3c46b65c
Rizqy EP <rizqyepr@gmail.com>
2020-04-30 22:09:52 +0700

    sans boi

--- geladi.py
+++ geladi.py
@@ -1,12 +1,13 @@
 import time
 from time import sleep
 from selenium import webdriver
-URL = "https://igracias.telkomuniversity.ac.id/index.php?pageid=2941"
+URL = "https://igracias.telkomuniversity.ac.id/index.php"
  
 PATH_TO_DRIVER = 'chromedriver_win32/chromedriver.exe'
 browser = webdriver.Chrome(PATH_TO_DRIVER)
 browser.set_window_position(0,0)
 browser.get(URL)
+
 while True:
-    sleep(10)
+    sleep(5) #atur sesuai waktu yang diperluin buat login diawal
     browser.refresh()
