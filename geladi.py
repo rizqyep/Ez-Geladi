2de74e4c4469b21165cbc5139ce941f60cf4630d
Rizqy EP <rizqyepr@gmail.com>
2020-04-30 23:02:19 +0700

    sleepy

--- geladi.py
+++ geladi.py
@@ -6,7 +6,7 @@ PATH_TO_DRIVER = 'chromedriver_win32/chromedriver.exe'
 browser = webdriver.Chrome(PATH_TO_DRIVER)
 
 browser.get(URL)
-
+sleep(5)
 while True:
     browser.get("https://igracias.telkomuniversity.ac.id/geladi")
     sleep(10) #atur sesuai waktu yang diperluin buat login diawal
