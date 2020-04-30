e9c23a8e200a6bd52d67658f1dcf8a2695a5d0c6
Rizqy EP <rizqyepr@gmail.com>
2020-04-30 23:43:29 +0700

    geladikuy

--- geladi.py
+++ geladi.py
@@ -7,8 +7,8 @@ browser = webdriver.Chrome(PATH_TO_DRIVER)
 
 browser.get(URL)
 sleep(20) #atur sesuai waktu yang diperluin buat login diawal
-browser.get("https://igracias.telkomuniversity.ac.id/geladi/index.php")
+browser.get("https://igracias.telkomuniversity.ac.id/geladi?pageid=20391")
    
 while True:
-    sleep(10)
+    sleep(5)
     browser.refresh()
